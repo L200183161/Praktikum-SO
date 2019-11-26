633b43b3d2cbd3ba335e3e74e0ee7fec6de76bf7
Donny Rizal Adhi Pratama <44853905+L200183161@users.noreply.github.com>
2019-11-26 13:44:12 +0700

    Add files via upload

--- Modul_8/exec.c
+++ Modul_8/exec.c
@@ -1,35 +1,32 @@
-#include <wait.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 int main(int argc, char*argv[]){
-
 	pid_t pid;
 	int i;
-	
-	if (argc != 3)
+
+	if(argc != 3)
 	{
 		printf("\nInsufficient arguments to load program");
-		printf("\nUsage: ./a.out <path> <cmd>\n");exit(-1);
+		printf("\nUsage: ./a.out <path> <cmd>\n"); exit (-1);
 	}
-
 	switch(pid = fork())
 	{
 	case -1:
-		printf("Fork failed");
+		printf("fork failed");
 		exit(-1);
 	case 0:
-		printf("Child process\n");
-		i = execl(argv[1], argv[2], NULL);
-		if (i < 0)
+		printf("child process\n");
+		i = execl(argv[1], argv[2], 0);
+		if(i < 0)
 		{
-			printf("%s program not loaded exec system call\n", argv[2]);
+			printf("%s program not loaded using exec system call\n", argv[2]);
 			exit(-1);
 		}
 	default:
 		wait(NULL);
-		printf("Child Terminated\n");
+		printf("child terminated\n");
 		exit(0);
 	}
 }
--- Modul_8/wait.c
+++ Modul_8/wait.c
@@ -7,24 +7,23 @@ int main(){
 	int i, status;
 	pid_t pid;
 	pid = fork();
-
-	if (pid , 0){
+	if(pid < 0){
 		printf("\nPembuatan proses gagal\n");
 		exit(-1);
-	}
-	else if(pid > 0)
+	}	
+	else if (pid > 0)
 	{
 		wait(NULL);
-		printf ("\nParent starts\nNomor Genap:");
-		for (i=2;i<=10;i+=2)
-			printf ("%3d" ,i);
-		printf("\nParent ends\n");																							printf("\nParent ends\n");
+		printf("\nParent starts\nNomor genap:");
+		for (i=2; i<=10;i+=2)
+			printf("%3d", i);
+		printf("\nParent ends\n");
 	}
-	else if (pid == 0)
+	else if(pid == 0)
 	{
-		printf ("Child starts\nNomor Ganjil:");
-		for (i=1;i<10;i+=2)
-			printf ("%3d" ,i);
-		printf ("\nChild ends\n");
+		printf("Child start\nNomor ganjil:");
+		for(i=1;i<10;i+=2)
+			printf("%3d", i);
+		printf("\nChild ends\n");
 	}
 }
