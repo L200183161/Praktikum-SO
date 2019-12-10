bc45c9568a534a29a4cd2752faccb43814170071
Donny Rizal Adhi Pratama <44853905+L200183161@users.noreply.github.com>
2019-12-10 13:18:21 +0700

    Add files via upload

--- Modul 10/del.c
+++ Modul 10/del.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-int main(int argc, char const *argv[])
-{
+
+int main(int argc, char* argv[]) {
 	int fd;
-	if(argc != 2){
-		printf("Usage : gc del.c -o del\n");
-		printf("Usage : .del <filename>\n");
+	if (argc != 2)
+	{
+		printf("Usage: gcc del.c -o del\n");
+		printf("Usage: ./del <filename>\n");
 		exit(-1);
 	}
 	fd = open(argv[1], O_RDONLY);
-	if(fd != -1){
+	if (fd != -1)
+	{
 		close(fd);
 		unlink(argv[1]);
-	}
-	else
+	} else
 		perror(argv[1]);
-	return 0;
 }
